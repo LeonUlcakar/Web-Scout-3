3b37b4d2b4be50405ce4b27c0e8fbe65c38cad5a
LeonUlcakar <67747860+LeonUlcakar@users.noreply.github.com>
2020-07-06 14:56:47 +0200

    Add files via upload

---
+++ Web Scout v0.13/Web Scout3/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Web_Scout3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Back = new System.Windows.Forms.Button();
+            this.Foreward = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.NewTab = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.Home = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.CloseAllBut = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(12, 27);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(30, 23);
+            this.Back.TabIndex = 0;
+            this.Back.Text = "<--";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Foreward
+            // 
+            this.Foreward.Location = new System.Drawing.Point(48, 27);
+            this.Foreward.Name = "Foreward";
+            this.Foreward.Size = new System.Drawing.Size(30, 23);
+            this.Foreward.TabIndex = 1;
+            this.Foreward.Text = "-->";
+            this.Foreward.UseVisualStyleBackColor = true;
+            this.Foreward.Click += new System.EventHandler(this.Foreward_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(84, 27);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(30, 23);
+            this.Refresh.TabIndex = 2;
+            this.Refresh.Text = "R";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // NewTab
+            // 
+            this.NewTab.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NewTab.Location = new System.Drawing.Point(848, 27);
+            this.NewTab.Name = "NewTab";
+            this.NewTab.Size = new System.Drawing.Size(63, 23);
+            this.NewTab.TabIndex = 3;
+            this.NewTab.Text = "New Tab";
+            this.NewTab.UseVisualStyleBackColor = true;
+            this.NewTab.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Search
+            // 
+            this.Search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Search.Location = new System.Drawing.Point(747, 27);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(30, 23);
+            this.Search.TabIndex = 4;
+            this.Search.Text = "Ser";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // Home
+            // 
+            this.Home.Location = new System.Drawing.Point(120, 27);
+            this.Home.Name = "Home";
+            this.Home.Size = new System.Drawing.Size(30, 23);
+            this.Home.TabIndex = 5;
+            this.Home.Text = "H";
+            this.Home.UseVisualStyleBackColor = true;
+            this.Home.Click += new System.EventHandler(this.Home_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Stop.Location = new System.Drawing.Point(783, 27);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(69, 23);
+            this.Stop.TabIndex = 6;
+            this.Stop.Text = "Close Tab";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(981, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(156, 26);
+            this.textBox1.MinimumSize = new System.Drawing.Size(40, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(585, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 55);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(981, 501);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(973, 475);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(967, 469);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // CloseAllBut
+            // 
+            this.CloseAllBut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseAllBut.Location = new System.Drawing.Point(920, 27);
+            this.CloseAllBut.Name = "CloseAllBut";
+            this.CloseAllBut.Size = new System.Drawing.Size(57, 23);
+            this.CloseAllBut.TabIndex = 10;
+            this.CloseAllBut.Text = "CloseAll";
+            this.CloseAllBut.UseVisualStyleBackColor = true;
+            this.CloseAllBut.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 557);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Home);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.NewTab);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Foreward);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.CloseAllBut);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Web Scout";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Foreward;
+        private new System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Button NewTab;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Home;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button CloseAllBut;
+    }
+}
+
